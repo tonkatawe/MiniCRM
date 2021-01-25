603f2bae73408ac4e586d64f3c1553a464b238ef
tonkatawe <vlahov.anton@gmail.com>
2021-01-25 13:04:13 +0200

    Implement user roles view model

---
+++ Web/MiniCRM.Web.ViewModels/Administration/UserRoles/UserRolesViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace MiniCRM.Web.ViewModels.Administration.UserRoles
+{
+    using System.Collections.Generic;
+
+    using MiniCRM.Data.Models;
+    using MiniCRM.Services.Mapping;
+
+    public class UserRolesViewModel : IMapFrom<ApplicationUser>
+    {
+        public string UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerator<string> Roles { get; set; }
+    }
+}
