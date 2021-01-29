26ad4e388ec479b82ee3da68557f28381e9615ef
tonkatawe <vlahov.anton@gmail.com>
2021-01-29 07:28:00 +0200

    add products controller, models, views

---
+++ Web/MiniCRM.Web/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MiniCRM.Web.Infrastructure;
+using MiniCRM.Web.ViewModels.Products;
+
+namespace MiniCRM.Web.Controllers
+{
+    public class ProductsController:Controller
+    {
+        public ProductsController()
+        {
+
+        }
+
+        public async Task<IActionResult> Index(int? pageNumber)
+        {
+            return View(await PaginatedList<ProductViewModel>.CreateAsync(products, pageNumber ?? 1, pageSize));
+        }
+
+        [Authorize(Roles = ("Owner, Administrator"))]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = ("Owner, Administrator"))]
+        public async Task<IActionResult> Create(ProductCreateModel input)
+        {
+
+        }
+    }
+}
