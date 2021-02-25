cc87b6ae7b16763d71a9298a7cb8059c0f4e07e7
tonkatawe <vlahov.anton@gmail.com>
2021-02-25 12:30:12 +0200

    migrate create sale js logic into a file

---
+++ Web/MiniCRM.Web/wwwroot/js/CreateSale.js
@@ -0,0 +1,34 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    window.stepper = new window.Stepper(document.querySelector('.bs-stepper'));
+});
+var slim = new SlimSelect({
+    select: '#demo',
+    placeholder: "Choose product to add",
+    searchText: "It is not available product",
+    searchPlaceholder: "Search for available product"
+
+});
+$(function () {
+    $("#btnNext").click(function () {
+        var ids = $("#demo").val();
+        var customerId = $("#customerId").val();
+        $.ajax({
+            type: "post",
+            url: "/Employees/Sales/SaleProductPartial",
+            data: { "ids": ids, "customerId": customerId },
+            success: function (result) {
+                $("#container").append(result);
+            },
+            error: function (ex) {
+                alert("Error");
+            }
+        });
+    });
+});
+$(function () {
+    $("#btnPrevious").click(function () {
+
+        $("#container").empty();
+
+    });
+});
